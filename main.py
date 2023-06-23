12e40506a9d4ffebf95e39d1eaf1a60442b17092
Peagah-Vieira <peagahvieira2003@gmail.com>
2023-06-23 09:36:44 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter
+from tkinter import ttk
+
+
+class app(ttk.Frame):
+    def __init__(self, parent):
+        return
+
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    root.title("Currency Converter")
+
+    root.mainloop()
